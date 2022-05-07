b05f100075534e9a3ca334f0a388921519c8b463
Yiyang Lu <lyiyang@ucsc.edu>
2022-05-06 20:48:17 -0700

    initial commit

---
+++ src/prefabs/Enemies.js
@@ -0,0 +1,7 @@
+class Enemies extends Phaser.GameObjects.Sprite {
+    constructor (scene,x,y,texture,frame){
+        super (scene,x,y,texture,frame);
+    
+        scene.add.existing (this);
+    }
+}

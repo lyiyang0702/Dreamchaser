6a4ed79b69cecf8c2420c363d88db6194fd5b828
Thrisha Kopula <thrishak1@gmail.com>
2022-05-13 19:50:32 -0700

    added jungle

--- src/scenes/Level_2.js
+++ src/scenes/Level_2.js
@@ -2,9 +2,13 @@ class LEVEL_2 extends Phaser.Scene {
     constructor () {
         super ("level_2");
     }
-    
 
-
+    preload() {
+        // set load path
+        this.load.path = 'assets/';
+        // take care of all of our asset loading now
+        this.load.image('jungle', 'jungleBackground.png');
+    }
     create() {
         let menuConfig = {
             fontFamily: 'Courier',
@@ -18,6 +22,7 @@ class LEVEL_2 extends Phaser.Scene {
             },
             fixedWidth: 0
         }
+        this.jungle = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'jungle').setOrigin(0, 0);
         this.add.text (game.config.width/2, game.config.height/2 - borderUISize - borderPadding*5, 'LEVEL 2',menuConfig).setOrigin(0.5);
         this.player = new Player(this, game.config.width/2, game.config.height/2,'cat_atlas', 'idle_down_0001', MAX_JUMP);
         this.player.create();
